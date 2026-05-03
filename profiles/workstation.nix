113ddf6f0131e5c0a86c71fb8b277ae6ab7604ab
FjellOverflow <fjelloverflow@protonmail.com>
2026-05-03 22:14:47 +0200

    dark mode

--- profiles/workstation.nix
+++ profiles/workstation.nix
@@ -102,6 +102,7 @@
     dconf.settings."org/gnome/desktop/interface" = {
       clock-format = "24h";
       clock-show-weekday = true;
+      color-scheme = "prefer-dark";
     };
 
     dconf.settings."org/gnome/desktop/calendar" = {
