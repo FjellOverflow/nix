d647bcd358bea548092b14ddfde09f9e761b95b7
FjellOverflow <fjelloverflow@protonmail.com>
2026-05-03 14:23:41 +0200

    define all apps

--- profiles/workstation.nix
+++ profiles/workstation.nix
@@ -4,16 +4,14 @@
   networking.networkmanager.enable = true;
 
   services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
   services.xserver.xkb = {
     layout = "no";
     variant = "";
   };
 
   console.keyMap = "no";
-
-  services.printing.enable = true;
 
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -22,6 +20,60 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # jack.enable = true;
+  };
+
+  services.gnome.core-apps.enable = false;
+
+  documentation.nixos.enable = false;
+
+  environment.systemPackages = with pkgs; [
+    baobab
+    decibels
+    deja-dup
+    gnome-boxes
+    gnome-calculator
+    gnome-connections
+    gnome-font-viewer
+    gnome-disk-utility
+    gnome-logs
+    gnome-system-monitor
+    gnome-text-editor
+    gnome-tweaks
+    gparted
+    loupe
+    mullvad-vpn
+    nautilus
+    papers
+    ptyxis
+    showtime
+    snapshot
+    syncthing
+    vscode
+  ];
+
+  environment.gnome.excludePackages = with pkgs; [
+    gnome-tour
+  ];
+
+  services.xserver.excludePackages = with pkgs; 
+  [
+    xterm
+  ];
+
+  services.flatpak = {
+    enable = true;
+    packages = [
+      "com.bitwarden.desktop"
+      "com.brave.Browser"
+      "com.mattjakeman.ExtensionManager"
+      "com.spotify.Client"
+      "com.usebruno.Bruno"
+      "md.obsidian.Obsidian"
+      "org.gimp.GIMP"
+      "org.freefilesync.FreeFileSync"
+      "org.inkscape.Inkscape"
+      "org.libreoffice.LibreOffice"
+      "org.videolan.VLC"
+    ];
   };
 }
