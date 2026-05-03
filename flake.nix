9fa8f2ebf8ef3a11d3206612c9bc356a4e5a0f72
FjellOverflow <fjelloverflow@protonmail.com>
2026-05-03 14:23:03 +0200

    enable flatpaks

--- flake.nix
+++ flake.nix
@@ -2,11 +2,13 @@
   description = "Snøflak NixOS configurations";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  inputs.nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=v0.7.0";
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, nix-flatpak }: {
     nixosConfigurations = {
       vm = nixpkgs.lib.nixosSystem {
         modules = [
+          nix-flatpak.nixosModules.nix-flatpak
           ./common.nix
           ./machines/vm/default.nix
         ];
