ee4ad46a7ac8f9d92491e97c3393292055c48a60
FjellOverflow <fjelloverflow@protonmail.com>
2026-05-23 21:33:06 +0200

    fix broken syncthing config

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -5,6 +5,7 @@
     enable = true;
     user = user;
     dataDir = "/home/${user}/Syncthing";
+    configDir = "/home/${user}/.config/syncthing";
   };
   environment.systemPackages = with pkgs; [ syncthing ];
 }
