3eafa9ab71a8a1cba6902c17c29b8ef592bee566
FjellOverflow <fjelloverflow@protonmail.com>
2026-07-09 12:57:37 +0200

    fix tailscale breaking DNS on resume

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -6,5 +6,6 @@
     openFirewall = true;
   };
 
-  powerManagement.resumeCommands = "systemctl restart tailscaled";
+  # keeps MagicDNS from breaking on resume: https://github.com/tailscale/tailscale/issues/4254
+  services.resolved.enable = true;
 }
